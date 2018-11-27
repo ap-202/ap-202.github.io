1b7137f3640d23bf442e411234c3ac3774a1ba44
ap-202 <aakash08852@gmail.com>
2018-11-26 19:26:57 -0500

    Add files via upload

--- ReadData.js
+++ ReadData.js
@@ -1,10 +1,32 @@
-var databaseInstance=firebase.database();
-
-function dataRetreived(data){
+var areas=[]; var hostNames=[]; var upTimes=[]; var versions=[]; var finalData; var keys;
+var config = {
+	apiKey: "AIzaSyDj3WmU4bQvtI65DYOlllFIv3-alBXYi0E",
+	authDomain: "bloomberg-project.firebaseapp.com",
+	databaseURL: "https://bloomberg-project.firebaseio.com",
+	projectId: "bloomberg-project",
+	storageBucket: "bloomberg-project.appspot.com",
+	messagingSenderId: "267927372179"
+};
+firebase.initializeApp(config);
+console.log(firebase);
+var databaseInstance = firebase.database();
+var databaseReference = databaseInstance.ref();
+databaseReference.on("value", dataRetrieved, dataException);
+function dataRetrieved(data){
 	console.log(data.val());
+	finalData=data.val();
+	keys=Object.keys(finalData);
+	for(var x=0;x<keys.length;x++){
+		try{areas.push(finalData[x].area);}catch(e){console.log(e);areas.push("");}
+		try{hostNames.push(finalData[x].hostname);}catch(e){console.log(e);hostNames.push("");}
+		try{upTimes.push(finalData[x].uptime);}catch(e){console.log(e);upTimes.push("");}
+		try{versions.push(finalData[x].version);}catch(e){console.log(e);versions.push("");}
+	}
+	console.log(areas);
+	console.log(hostNames);
+	console.log(upTimes);
+	console.log(versions);
 }
-
-function dataException(err){
-	console.log(err);
+function dataException(e){
+console.log(e);
 }
-var ref=database.ref('area' , dataRetreived, dataException)
