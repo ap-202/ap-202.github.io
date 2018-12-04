053b573a5637a426fa2af91e7bbab0c986164c54
ap-202 <aakash08852@gmail.com>
2018-12-04 08:05:33 -0500

    Formatting

--- ReadData.js
+++ ReadData.js
@@ -1,4 +1,11 @@
-var areas=[]; var hostNames=[]; var upTimes=[]; var versions=[]; var finalData; var keys;
+var areas=[]; 
+var hostNames=[]; 
+var upTimes=[]; 
+var versions=[]; 
+var finalData;
+var tableData=[];
+var keys;
+
 var config = {
 	apiKey: "AIzaSyDj3WmU4bQvtI65DYOlllFIv3-alBXYi0E",
 	authDomain: "bloomberg-project.firebaseapp.com",
@@ -7,34 +14,46 @@ var config = {
 	storageBucket: "bloomberg-project.appspot.com",
 	messagingSenderId: "267927372179"
 };
+
 firebase.initializeApp(config);
 console.log(firebase);
-var databaseInstance = firebase.database();
-var databaseReference = databaseInstance.ref();
+var databaseInstance = firebase.database();var databaseReference = databaseInstance.ref();
 databaseReference.on("value", dataRetrieved, dataException);
-var finalData2=[];
+
 function dataRetrieved(data){
 	console.log(data.val());
 	finalData=data.val();
 	keys=Object.keys(finalData);
 	for(var x=0;x<keys.length;x++){
-		try{areas.push(finalData[x].area);}catch(e){console.log(e);areas.push("");}
-		try{hostNames.push(finalData[x].hostname);}catch(e){console.log(e);hostNames.push("");}
-		try{upTimes.push(finalData[x].uptime);}catch(e){console.log(e);upTimes.push("");}
-		try{versions.push(finalData[x].version);}catch(e){console.log(e);versions.push("");}
+		try{areas.push(finalData[x].area);
+		   }catch(e){
+			   console.log(e);
+			   areas.push("");
+		   }
+		try{hostNames.push(finalData[x].hostname);
+		   }catch(e){
+			   console.log(e);
+			   hostNames.push("");
+		   }
+		try{upTimes.push(finalData[x].uptime);
+		   }catch(e){
+			   console.log(e);
+			   upTimes.push("");
+		   }
+		try{versions.push(finalData[x].version);
+		   }catch(e){
+			   console.log(e);
+			   versions.push("");
+		   }
 	}
-	console.log(areas);
-	console.log(hostNames);
-	console.log(upTimes);
-	console.log(versions);
-	
+	console.log(areas);console.log(hostNames);console.log(upTimes);console.log(versions);
 	for(var y=0;y<keys.length-1;y++){
-		finalData2.push([areas[y],hostNames[y],upTimes[y],versions[y]]);
+		tableData.push([areas[y],hostNames[y],upTimes[y],versions[y]]);
 	}
-	console.log(finalData2);
+	console.log(tableData);
 	$(document).ready(function() {
 	    $("#table1").DataTable( {
-		data: finalData2,
+		data: tableData,
 		"ordering": false,
 		columns: [
 		    { title: "area" },
@@ -45,6 +64,4 @@ function dataRetrieved(data){
 	    } );
 	} );
 }
-function dataException(e){
-	console.log(e);
-}
+function dataException(e){console.log(e);}
